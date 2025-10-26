b49d1d1f1e73c6c8b5e5fd55dd5295e30873678e
Prabhav Kumar <prabhavkyamsaria5@gmail.com>
2025-10-26 23:07:00 +0530

    Update BASE_URL for Retrofit instance

--- app/src/main/java/com/deecode/walls/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/deecode/walls/data/remote/RetrofitInstance.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "https://raga-api.vercel.app/"
+    private const val BASE_URL = "https://actress-gallery-api-production.up.railway.app/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
